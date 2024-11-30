459ced92485049677df240c31f64afc3add2db1f
opositor1 <opositor366@gmail.com>
2024-11-30 14:39:30 -0400

    Add files via upload
    agregar

---
+++ search.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>search result</title>
+<meta charset="utf-8">    
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="fonts/font-awesome.css">
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="search/search.js"></script>
+<script src="js/jquery.mobilemenu.js"></script> 
+<script src="js/jquery.ui.totop.js"></script>
+<script src="js/jquery.equalheights.js"></script> 
+<!--[if lt IE 9]>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+<!--header-->
+<header>
+    <div class="follow-box">
+        <div class="container">
+            <ul>
+                <li><a href="#" class="fa fa-twitter"></a></li>
+                <li><a href="#" class="fa fa-google-plus"></a></li>
+                <li><a href="#" class="fa fa-facebook"></a></li>
+                <li><a href="#" class="fa fa-skype"></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <h1 class="navbar-brand navbar-brand_"><a href="index.html">Veterinarian blog</a></h1>
+        <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+            <ul class="nav sf-menu clearfix">
+                <li><a href="index.html">home</a></li>
+                <li class="sub-menu"><a href="index-1.html">gallery</a><span></span>
+                    <ul class="submenu">
+        				<li><a href="#">Aliquam congue</a><span></span>
+                          <ul class="submenu">
+                            <li><a href="#">Sed in lacus ut</a></li>
+                            <li><a href="#">Enim adipiscing</a></li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Sit amet</a></li>
+                          </ul>  
+                        </li>
+        				<li><a href="#">Fermentum nisl</a></li>
+        				<li><a href="#">Mauris accumsan</a></li>
+                        <li><a href="#">Nulla vel diam</a></li>
+        			</ul>
+                </li>
+                <li><a href="index-2.html">blog</a></li>
+                <li><a href="index-3.html">about</a></li>
+                <li><a href="index-4.html">contacts</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div class="global indent">
+    <!--content-->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <h2>Search result:<em></em></h2>
+                <div id="search-results"></div>
+            </div>
+        </div>
+    </div>
+<div class="sub_foo">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 maxheight">
+                <h2 class="clr4">Related Links<em></em></h2>
+                <ul class="list1">
+                    <li><a href="#">Interdum vitae,dapibus ac</a></li>
+                    <li><a href="#">Scelerisque vitae, pede</a></li>
+                    <li><a href="#">Donec eget tellus non erat</a></li>
+                    <li><a href="#">Lacinia fermentum</a></li>
+                    <li><a href="#">Donec in velit vel ipsum auctor pulvinar</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 maxheight">
+                <h2 class="clr5">Centers of Excellence<em></em></h2>
+                <ul class="list1">
+                    <li><a href="#">Dog Care Center</a></li>
+                    <li><a href="#">Cat Care Center</a></li>
+                    <li><a href="#">Nutrition Center</a></li>
+                    <li><a href="#">Video Center</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 maxheight">
+                <h2 class="clr4">Medicine<em></em></h2>
+                <ul class="list1">
+                    <li><a href="#">Internal Medicine</a></li>
+                    <li><a href="#">Alternative Medicine</a></li>
+                    <li><a href="#">Anesthesia</a></li>
+                    <li><a href="#">Cardiology</a></li>
+                    <li><a href="#">Clinical Pathology</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 maxheight">
+                <h2 class="clr5">Archives<em></em></h2>
+                <ul class="list1">
+                    <li><a href="#">May 2014</a></li>
+                    <li><a href="#">April 2014</a></li>
+                    <li><a href="#">March 2014</a></li>
+                    <li><a href="#">February 2014</a></li>
+                    <li><a href="#">January 2014</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!--footer-->
+<footer>
+    <div class="container">
+        <p>Veterinarian blog &copy; <em id="copyright-year"></em> All Rights Reserved <span>|</span><br> <a href="index-5.html">Privacy policy</a></p>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
